4506d4d6f2d05ccdc50aa5bcacb8ac3704ea5eb0
RubenVG02 <rubenvillegas2@gmail.com>
2023-01-28 16:12:37 +0100

    main predictor using input file

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from predictor import predict_lifetime
-import mega
+from mega import Mega
 
 def upload_to_mega(file):
     # This function will upload the file to mega.nz
-    mega=mega.Mega()
+    mega=Mega()
     mega.login()
     file=mega.upload(file)
     link=mega.get_upload_link(file)
@@ -11,23 +11,35 @@ def upload_to_mega(file):
 
  
  
-def default_file(name_file="sequences.txt"):
+def default_file(output_file="sequences.csv"):
     #Function to create a default file for the output
-    with open(name_file,"w") as file:
-        file.write("sequence,lifetime") 
+    with open(output_file,"w") as file:
+        file.write("sequence,lifetime(hours)") 
 
-def predictor(organism="", target="", upload_mega=True, name_file="sequences.txt"):
+def predictor(organism="yeast", upload_mega=True, output_file="sequences.csv", input_file="input.txt"):
     # This function will call the predict_lifetime function from predictor.py
     # and will return the lifetime of the protein
     # It will also upload the file to mega.nz
     default_file()
-    with open(name_file,"a") as f:
-        for target in f:
-            lifetime=predict_lifetime(organism, target)
-            f.write(f"{target},{lifetime}")
+    sequences=[]
+    lifetimes=[]
+    with open(input_file,"r") as f:
+        for sequence in f:
+            sequence=sequence.replace("\n","")
+            sequences.append(sequence)
+            lifetime=predict_lifetime(organism, sequence)
+            lifetimes.append(lifetime)
+    results=list(zip(sequences,lifetimes))
+    with open(output_file,"a", newline="") as f:
+        for i in range(len(results)):
+            f.write(f"\n{sequences[i]},{lifetimes[i]:.2f}")
 
-    if upload_mega==True:
-        upload_to_mega("results.csv")
+            
+    
     
 
-default_file()
+    if upload_mega==True:
+        upload_to_mega("sequences.csv")
+    
+
+predictor()
