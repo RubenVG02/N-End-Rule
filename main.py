f00c362e178ee6b756aa344d1694bb21c302b3a2
RubenVG02 <rubenvillegas2@gmail.com>
2023-01-28 13:10:38 +0100

    main function

---
+++ main.py
@@ -0,0 +1,31 @@
+from predictor import predict_lifetime
+import mega
+
+def upload_to_mega(file):
+    # This function will upload the file to mega.nz
+    mega=mega.Mega()
+    mega.login()
+    mega.upload(file)
+ 
+ 
+def default_file(name_file="sequences.txt"):
+    #Function to create a default file for the output
+    with open(name_file,"w") as file:
+        file.write("sequence,lifetime") 
+
+def predictor(organism="", target="", upload_mega=True, name_file="sequences.txt"):
+    # This function will call the predict_lifetime function from predictor.py
+    # and will return the lifetime of the protein
+    # It will also upload the file to mega.nz
+    default_file()
+    with open(name_file,"a") as f:
+        for target in f:
+            lifetime=predict_lifetime(organism, target)
+            f.write(f"{target},{lifetime}")
+
+    if upload_mega==True:
+        upload_to_mega("results.csv")
+    
+    return lifetime
+
+default_file()
