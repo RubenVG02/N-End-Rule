210f797fae8e6f8c8db5c8a19f3a28d977432777
RubenVG02 <rubenvillegas2@gmail.com>
2023-01-28 01:25:38 +0100

    done

--- main.py
+++ main.py
@@ -15,9 +15,7 @@ elements_yeast={"M":1200,
                    "F":3,
                    "D":3,
                    "K":3,
-                   "R":2
-                   
-                   }
+                   "R":2}
 
 elements_mammals={"M":30,
                    "G":30,
@@ -40,6 +38,49 @@ elements_mammals={"M":30,
                    "S":1.9,
                    "N":1.4,
                    "C":1.2,
-                   "Q":0.8          
-                   
-                   }
+                   "Q":0.8}
+
+elements_bacteria={"V":600,
+                   "M":600,
+                   "G":600,
+                   "P":600,
+                   "I":600,
+                   "T":600,
+                   "L":2,
+                   "A":600,
+                   "H":600,
+                   "W":2,
+                   "Y":2,
+                   "S":600,
+                   "N":600,
+                   "K":2,
+                   "C":600,
+                   "D":600,
+                   "F":2,
+                   "E":600,
+                   "R":2,
+                   "Q":600} 
+
+def predict_lifetime(organism="", target=""):
+    organisms=["yeast","mammals","bacteria"]
+    last_aa=target[-1]
+    lifetime=0
+    if organism.lower()==organisms[0]:
+        for i in elements_yeast:
+            if i==last_aa:
+                lifetime=(elements_mammals[i]*2)/60
+    elif organism==organisms[1]:
+        for i in elements_mammals:
+            if i==last_aa:
+                lifetime=elements_mammals[i]*2
+    elif organism==organisms[2]:
+        for i in elements_bacteria:
+            if i==last_aa:
+                lifetime=(elements_bacteria[i]*2)/60
+    else:
+        print("Organism not found")
+        
+    print(f"The predicted lifetime of the protein is: {lifetime} hours")
+    
+
+predict_lifetime("yeast","SWDEFVDRSVQLFRADPESTRYVMKYRHCDGKLVLKVTDNKECLKFKTDQAQEAKKMEKLNNIFFTLM")
